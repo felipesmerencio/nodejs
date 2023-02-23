68ba6f40bc9658f3d95a8599ca29a44297065464
merencio <felipe.smerencio@gmail.com>
2023-02-23 06:58:56 -0300

    backup das ultimas alterações

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,8 @@ import 'dotenv/config';
 import { app } from './app';
 import { dataSource } from '../typeorm';
 
-// TODO: correct the fail onfulfilled
-
-dataSource.initialize().then(onfulfilled:() => {
+//dataSource.initialize().then(onfulfilled:() => {
+dataSource.initialize().then(() => {
   app.listen(3333, () => {
     console.log('Server started on port 3333!')
   });
